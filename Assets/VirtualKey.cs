44bc9de43140cc39892ccc26d59fe2c183498719
Paul Sinnett <paul.sinnett@gmail.com>
2021-10-08 00:41:29 +0100

    handle keyboard events

--- Assets/VirtualKey.cs
+++ Assets/VirtualKey.cs
@@ -2,26 +2,72 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class VirtualKey : MonoBehaviour
 {
+    RectTransform rectTransform;
     public KeyCode code;
+    public string unshifted;
+    public string shifted;
+    public TextMeshProUGUI keyCap;
 
     VirtualKeyboard keyboard;
+    bool shift = false;
 
     void Start()
     {
         keyboard = GetComponentInParent<VirtualKeyboard>();
+        keyCap.text = unshifted;
+        rectTransform = GetComponent<RectTransform>();
+        rectTransform.sizeDelta += Vector2.right * keyCap.preferredWidth;
     }
 
     public void OnClick()
     {
-        keyboard.Type(this);
+        switch (code)
+        {
+            case KeyCode.CapsLock:
+                keyboard.ToggleCapsLock();
+                break;
+
+            case KeyCode.Return:
+                keyboard.Return();
+                break;
+
+            case KeyCode.LeftShift:
+            case KeyCode.RightShift:
+                keyboard.ToggleShift();
+                break;
+
+            default:
+                keyboard.Type(this);
+                break;
+        }
     }
 
     public void Modify(TMP_InputField field)
     {
-        field.SetTextWithoutNotify(field.text + code.ToString());
+        field.ProcessEvent(GetKeyboardEvent());
         field.ForceLabelUpdate();
+    }
+
+    Event GetKeyboardEvent()
+    {
+        Event keyboardEvent = Event.KeyboardEvent(shift ? ("#" + shifted) : shifted);
+        keyboardEvent.character = (char)(shift? shifted : unshifted)[0];
+        Debug.Log(keyboardEvent);
+        return keyboardEvent;
+    }
+
+    string GetKeyString()
+    {
+        return shift ? shifted : unshifted;
+    }
+
+    public void Shift(bool shift)
+    {
+        this.shift = shift;
+        keyCap.text = GetKeyString();
     }
 }
--- Assets/VirtualKeyboard.cs
+++ Assets/VirtualKeyboard.cs
@@ -4,21 +4,97 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Key
+{
+    public KeyCode code;
+    public string unshifted;
+    public string shifted;
+}
+
+[System.Serializable]
+public class Row
+{
+    public Key[] keys;
+}
+
 
 public class VirtualKeyboard : MonoBehaviour
 {
     public TMP_InputField inputField;
-    public GameObject firstKey;
+    public VirtualKey keyPrefab;
+    public HorizontalLayoutGroup rowPrefab;
+    public Row[] rows;
+
+    GameObject firstKey;
+    bool capsLock = false;
+    bool shift = false;
+    BaseEventData dummy;
+
+    void Start()
+    {
+        foreach (Row row in rows)
+        {
+            HorizontalLayoutGroup keyRow = Instantiate<HorizontalLayoutGroup>(rowPrefab, transform);
+            foreach (Key key in row.keys)
+            {
+                VirtualKey virtualKey = Instantiate<VirtualKey>(keyPrefab, keyRow.transform);
+                virtualKey.code = key.code;
+                virtualKey.shifted = key.shifted;
+                virtualKey.unshifted = key.unshifted;
+
+                if (firstKey == null)
+                {
+                    firstKey = virtualKey.gameObject;
+                }
+            }
+        }
+    }
+
+    public void Return()
+    {
+        gameObject.SetActive(false);
+    }
+
+    void UpdateShift()
+    {
+        BroadcastMessage("Shift", shift);
+    }
+
+    public void ToggleShift()
+    {
+        if (capsLock)
+        {
+            capsLock = false;
+        }
+        shift = !shift;
+        UpdateShift();
+    }
+
+    public void ToggleCapsLock()
+    {
+        capsLock = !capsLock;
+        shift = capsLock;
+        UpdateShift();
+    }
 
     public void Type(VirtualKey key)
     {
         key.Modify(inputField);
+        if (!capsLock && shift)
+        {
+            shift = false;
+            UpdateShift();
+        }
     }
+
 
     public void Open()
     {
         gameObject.SetActive(true);
-        Invoke("Select", 0.0f);
+        CanvasUpdateRegistry.RegisterCanvasElementForGraphicRebuild(inputField);
     }
 
     public void Select()
